424069298a998c70bdad26d505dcebd63cbc8e1c
m-nem-student <74405582+m-nem-student@users.noreply.github.com>
2024-02-25 12:06:07 +0100

    Split snake into logic and UI
    Split snake into SnakeLogic and SnakeUI

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,162 +1,242 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-using System.Xml.Serialization;
-///█ ■
-////https://www.youtube.com/watch?v=SGZgvMwjq2U
+using System;
+using static Snake.Program;
+using static System.Formats.Asn1.AsnWriter;
+
 namespace Snake
 {
-    class Program
+    static class Program
     {
-        static int screenWidth = Console.WindowWidth;
-        static int screenHeight = Console.WindowHeight;
-        static void Main(string[] args)
-        {
-            Console.WindowHeight = 16;
-            Console.WindowWidth = 32;
-            
-            Random randomNumber = new Random();
-            int score = 5;
-            bool isGameover = false;
-            pixel snakePosition = new pixel();
-            snakePosition.horizontalPosition = screenWidth/2;
-            snakePosition.verticalPosition = screenHeight/2;
-            snakePosition.snakeHeadeColor = ConsoleColor.Red;
-            string currentlMovementDirectio = "RIGHT";
-            List<int> horizontalPositionSnakeList = new List<int>();
-            List<int> verticalPositionSnakeList = new List<int>();
-            int horizontalBerrySpawnPosition = randomNumber.Next(0, screenWidth);
-            int verticalBerrySpawnPostion = randomNumber.Next(0, screenHeight);
-            
-            DateTime currentTime = DateTime.Now;
-            DateTime updateTime = DateTime.Now;
-            while (true)
-            {
-                Console.Clear();
 
-                isGameover = IsOutsideGameScreen(snakePosition);
-                CreateBorder();
 
-                Console.ForegroundColor = ConsoleColor.Green;
-                if (horizontalBerrySpawnPosition == snakePosition.horizontalPosition && verticalBerrySpawnPostion == snakePosition.verticalPosition)
-                {
-                    score++;
-                    horizontalBerrySpawnPosition = randomNumber.Next(1, screenWidth - 2);
-                    verticalBerrySpawnPostion = randomNumber.Next(1, screenHeight - 2);
-                }
-                for (int i = 0; i < horizontalPositionSnakeList.Count(); i++)
-                {
-                    Console.SetCursorPosition(horizontalPositionSnakeList[i], verticalPositionSnakeList[i]);
-                    Console.Write("■");
-                    if (horizontalPositionSnakeList[i] == snakePosition.horizontalPosition && verticalPositionSnakeList[i] == snakePosition.verticalPosition)
-                    {
-                        isGameover = true;
-                    }
-                }
-                if (isGameover == true)
-                {
-                    break;
-                }
-                Console.SetCursorPosition(snakePosition.horizontalPosition, snakePosition.verticalPosition);
-                Console.ForegroundColor = snakePosition.snakeHeadeColor;
-                Console.Write("■");
-                Console.SetCursorPosition(horizontalBerrySpawnPosition, verticalBerrySpawnPostion);
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("■");
-                currentTime = DateTime.Now;
-                while (true)
-                {
-                    updateTime = DateTime.Now;
-                    if (updateTime.Subtract(currentTime).TotalMilliseconds > 500) { break; }
-                    if (Console.KeyAvailable)
-                    {
-                        ConsoleKeyInfo pressedKey = Console.ReadKey(true);
-                        //Console.WriteLine(pressedKey.Key.ToString());
-                        if (pressedKey.Key.Equals(ConsoleKey.UpArrow))
-                        {
-                            currentlMovementDirectio = "UP";
-                        }
-                        if (pressedKey.Key.Equals(ConsoleKey.DownArrow))
-                        {
-                            currentlMovementDirectio = "DOWN";
-                        }
-                        if (pressedKey.Key.Equals(ConsoleKey.LeftArrow))
-                        {
-                            currentlMovementDirectio = "LEFT";
-                        }
-                        if (pressedKey.Key.Equals(ConsoleKey.RightArrow))
-                        {
-                            currentlMovementDirectio = "RIGHT";
-                        }
-                    }
-                }
-                horizontalPositionSnakeList.Add(snakePosition.horizontalPosition);
-                verticalPositionSnakeList.Add(snakePosition.verticalPosition);
-                switch (currentlMovementDirectio)
-                {
-                    case "UP":
-                        snakePosition.verticalPosition--;
-                        break;
-                    case "DOWN":
-                        snakePosition.verticalPosition++;
-                        break;
-                    case "LEFT":
-                        snakePosition.horizontalPosition--;
-                        break;
-                    case "RIGHT":
-                        snakePosition.horizontalPosition++;
-                        break;
-                }
-                if (horizontalPositionSnakeList.Count() > score)
-                {
-                    horizontalPositionSnakeList.RemoveAt(0);
-                    verticalPositionSnakeList.RemoveAt(0);
-                }
-            }
-            Console.SetCursorPosition(screenWidth / 5, screenHeight / 2);
-            Console.WriteLine("Game over, Score: "+ score);
-            Console.SetCursorPosition(screenWidth / 5, screenHeight / 2 +1);
-        }
+        // Constants for screen dimensions
+        const int _screenWidth = 32;
+        const int _screenHeight = 16;
+        public static int screenWidth {  get { return _screenWidth; } }
+        public static int screenHeight { get { return _screenHeight; } }
 
-        private static void CreateBorder()
-        {
-            for (int i = 0; i < screenWidth; i++)
-            {
-                Console.SetCursorPosition(i, 0);
-                Console.Write("■");
-                Console.SetCursorPosition(i, screenHeight - 1);
-                Console.Write("■");
-            }
-            for (int i = 0; i < screenHeight; i++)
-            {
-                Console.SetCursorPosition(0, i);
-                Console.Write("■");
-                Console.SetCursorPosition(screenWidth - 1, i);
-                Console.Write("■");
-            }
-        }
-
-        class pixel
+        public static int score = 5;
+        public static bool isNotGameOver = true;
+        public static string currentlMovementDirectio = "RIGHT";
+        public static Random random = new Random();
+        public static ConsoleColor UIcolor = ConsoleColor.Green;
+        public class SnakeBody
         {
             public int horizontalPosition { get; set; }
             public int verticalPosition { get; set; }
             public ConsoleColor snakeHeadeColor { get; set; }
         }
 
-        static bool IsOutsideGameScreen(pixel snakePosition)
+        public static List<int> horizontalPositionSnakeList = new List<int>();
+        public static List<int> verticalPositionSnakeList = new List<int>();
+        public static int horizontalBerrySpawnPosition;
+        public static int verticalBerrySpawnPosition;
+
+        public static DateTime currentTime;
+        public static DateTime updateTime;
+
+        static void Main(string[] args)
         {
-            if (snakePosition.horizontalPosition == screenWidth - 1 || snakePosition.horizontalPosition == 0 || snakePosition.verticalPosition == screenHeight - 1 || snakePosition.verticalPosition == 0)
+            Console.WindowHeight = screenHeight;
+            Console.WindowWidth = screenWidth;
+
+
+            SnakeBody snakePosition = new SnakeBody();
+            SnakeLogic.initializeSnake(snakePosition);
+            SnakeLogic.chooseBerryPozition();
+            SnakeLogic.startTimer();
+            while (isNotGameOver)
             {
-                return true;
+                Console.Clear();
+                SnakeLogic.isSnakeOutsideBorder(snakePosition);
+                SnakeUI.drawBorder();
+
+                Console.ForegroundColor = UIcolor;
+                if (SnakeLogic.isSnakeOnBarry(snakePosition))
+                {
+                    score++;
+                    SnakeLogic.chooseBerryPozition();
+                }
+                SnakeUI.drawSnake(snakePosition);
+                if (isNotGameOver == false)
+                {
+                    SnakeUI.drawGameOverText();
+                    break;
+                }
+                SnakeUI.drawSnakeHead(snakePosition);
+                SnakeUI.drawBerry();
+                SnakeLogic.selectSnakeDirection();
+                SnakeUI.addSnakeBodyPosition(snakePosition);
+                SnakeLogic.moveSnake(snakePosition);
+            }
+
+        }
+    }
+
+    static class SnakeUI
+    {
+        public static void drawBorder()
+        {
+            for (int i = 0; i < Program.screenWidth; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write("■");
+                Console.SetCursorPosition(i, Program.screenHeight - 1);
+                Console.Write("■");
+            }
+            for (int i = 0; i < Program.screenHeight; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("■");
+                Console.SetCursorPosition(Program.screenWidth - 1, i);
+                Console.Write("■");
+            }
+        }
+
+        public static void drawSnake(SnakeBody snakePosition)
+        {
+            for (int currentSnakeSquare = 0; currentSnakeSquare < Program.horizontalPositionSnakeList.Count(); currentSnakeSquare++)
+            {
+                Console.SetCursorPosition(Program.horizontalPositionSnakeList[currentSnakeSquare], Program.verticalPositionSnakeList[currentSnakeSquare]);
+                Console.Write("■");
+                if (SnakeLogic.isSnakeTouchingItSelf(snakePosition, currentSnakeSquare))
+                {
+                    Program.isNotGameOver = false;
+                }
+            }
+        }
+
+        public static void drawSnakeHead(SnakeBody snakePosition)
+        {
+            Console.SetCursorPosition(snakePosition.horizontalPosition, snakePosition.verticalPosition);
+            Console.ForegroundColor = snakePosition.snakeHeadeColor;
+            Console.Write("■");
+        }
+
+        public static void addSnakeBodyPosition(SnakeBody snakePosition)
+        {
+            Program.horizontalPositionSnakeList.Add(snakePosition.horizontalPosition);
+            Program.verticalPositionSnakeList.Add(snakePosition.verticalPosition);
+        }
+
+        public static void drawBerry()
+        {
+            Console.SetCursorPosition(Program.horizontalBerrySpawnPosition, Program.verticalBerrySpawnPosition);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("■");
+        }
+
+        public static void drawGameOverText()
+        {
+            Console.SetCursorPosition(screenWidth / 5, screenHeight / 2);
+            Console.WriteLine("Game over, Score: " + Program.score);
+            Console.SetCursorPosition(screenWidth / 5, screenHeight / 2 + 1);
+        }
+
+    }
+
+    static class SnakeLogic
+    {
+        public static void startTimer()
+        {
+            Program.currentTime = DateTime.Now;
+            Program.updateTime = DateTime.Now;
+        }
+
+        public static void chooseBerryPozition()
+        {
+            int spawnBerryMargin = 2;
+            horizontalBerrySpawnPosition = random.Next(0, screenWidth - spawnBerryMargin);
+            verticalBerrySpawnPosition = random.Next(0, screenHeight - spawnBerryMargin);
+        }
+
+
+        public static void initializeSnake(SnakeBody snakePosition)
+        {
+            snakePosition.horizontalPosition = screenWidth / 2;
+            snakePosition.verticalPosition = screenHeight / 2;
+            snakePosition.snakeHeadeColor = ConsoleColor.Red;
+        }
+
+
+        public static void selectSnakeDirection()
+        {
+            Program.currentTime = DateTime.Now;
+            while (true)
+            {
+                Program.updateTime = DateTime.Now;
+                if (Program.updateTime.Subtract(Program.currentTime).TotalMilliseconds > 500) { break; }
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+                    //Console.WriteLine(pressedKey.Key.ToString());
+                    if (pressedKey.Key.Equals(ConsoleKey.UpArrow))
+                    {
+                        currentlMovementDirectio = "UP";
+                    }
+                    if (pressedKey.Key.Equals(ConsoleKey.DownArrow))
+                    {
+                        currentlMovementDirectio = "DOWN";
+                    }
+                    if (pressedKey.Key.Equals(ConsoleKey.LeftArrow))
+                    {
+                        currentlMovementDirectio = "LEFT";
+                    }
+                    if (pressedKey.Key.Equals(ConsoleKey.RightArrow))
+                    {
+                        currentlMovementDirectio = "RIGHT";
+                    }
+                }
+            }
+        }
+
+        public static void moveSnake(SnakeBody snakePosition)
+        {
+            switch (currentlMovementDirectio)
+            {
+                case "UP":
+                    snakePosition.verticalPosition--;
+                    break;
+                case "DOWN":
+                    snakePosition.verticalPosition++;
+                    break;
+                case "LEFT":
+                    snakePosition.horizontalPosition--;
+                    break;
+                case "RIGHT":
+                    snakePosition.horizontalPosition++;
+                    break;
+            }
+            if (horizontalPositionSnakeList.Count() > score)
+            {
+                horizontalPositionSnakeList.RemoveAt(0);
+                verticalPositionSnakeList.RemoveAt(0);
+            }
+        }
+        public static bool isSnakeOutsideBorder(SnakeBody snakePosition)
+        {
+            if (snakePosition.horizontalPosition == screenWidth - 1 || snakePosition.horizontalPosition == 0
+                || snakePosition.verticalPosition == screenHeight - 1 || snakePosition.verticalPosition == 0)
+            {
+                isNotGameOver = false;
+                return false;
             }
             else
             {
-                return false;
+                isNotGameOver = true;
+                return true;
             }
         }
+
+        public static bool isSnakeOnBarry(SnakeBody snakePosition)
+        {
+            return horizontalBerrySpawnPosition == snakePosition.horizontalPosition && verticalBerrySpawnPosition == snakePosition.verticalPosition;
+        }
+
+        public static bool isSnakeTouchingItSelf(SnakeBody snakePosition, int i)
+        {
+            return horizontalPositionSnakeList[i] == snakePosition.horizontalPosition && verticalPositionSnakeList[i] == snakePosition.verticalPosition;
+        }
+
     }
+
 }
-//¦
